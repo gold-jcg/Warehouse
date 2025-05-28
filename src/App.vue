a1b4b92ec74e926291d3d1e290a4fe10bf5f643f
Chunguang Jin <13454759@qq.com>
2025-05-28 17:19:21 +0800

    pr

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,23 @@
  * @Author: Chunguang Jin 13454759@qq.com
  * @Date: 2025-05-26 17:36:15
  * @LastEditors: Chunguang Jin 13454759@qq.com
- * @LastEditTime: 2025-05-28 17:08:02
+ * @LastEditTime: 2025-05-28 17:18:26
  * @FilePath: /华东2/mydemo/src/App.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
-  
-  <router-view />
+  <div class="app">
+    <el-button>123</el-button>
+    <router-view />
+  </div>
+ 
 </template>
-
+<script>
+export default {
+  name: "appC",
+ data(){
+  
+ }
+};
+</script>
 <style lang="scss"></style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-
-createApp(App).use(store).use(router).mount('#app')
+/*
+ * @Author: Chunguang Jin 13454759@qq.com
+ * @Date: 2025-05-26 17:36:15
+ * @LastEditors: Chunguang Jin 13454759@qq.com
+ * @LastEditTime: 2025-05-28 17:16:27
+ * @FilePath: /华东2/mydemo/src/main.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import ElementPlus from "element-plus";
+import 'element-plus/dist/index.css'
+const app = createApp(App);
+app.use(ElementPlus).use(store).use(router).mount("#app");
